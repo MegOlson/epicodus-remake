6d571b6b202c04621fa75dd342176df3db0630f0
Guest User <Guest@epicodus-2.local>
2017-11-14 11:23:35 -0800

    Create relationship between section and lesson.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,23 +1,24 @@
 class LessonsController < ApplicationController
-
-
   def index
     @lessons = Lesson.all
   end
 
   def show
+    @section = Section.find(params[:section_id])
     @lesson = Lesson.find(params[:id])
   end
 
   def new
+    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @lesson = Lesson.new(lesson_params)
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.new(lesson_params)
     if @lesson.save
       flash[:notice] = "Lesson has been saved!"
-      redirect_to lessons_path
+      redirect_to section_path(@section)
     else
       render :new
     end
@@ -31,7 +32,7 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     if @lesson.update(lesson_params)
       flash[:notice] = "Your lesson as been updated!"
-      redirect_to lessons_path
+      redirect_to section_lessons_path
     else
       render :edit
     end
@@ -40,12 +41,12 @@ class LessonsController < ApplicationController
   def destroy
     @lesson = Lesson.find(params[:id])
     @lesson.destroy
-    redirect_to lessons_path
+    redirect_to section_lessons_path
   end
 
 private
   def lesson_params
-    params.require(:lesson).permit(:name, :content, :number)
+    params.require(:lesson).permit(:name, :content, :number, :section_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :lessons
-  resources :sections
+
+  root :to => 'sections#index'
+
+
+  resources :sections do
+    resources :lessons
+  end
 end
