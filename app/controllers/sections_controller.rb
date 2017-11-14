40b0f0c89451512b20a38b249235ff47341e3ef9
Kristen Kulha and Megan Olson <kristen+megan@>
2017-11-14 10:17:10 -0800

    Add new section and save.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,4 +3,29 @@ class SectionsController < ApplicationController
   def index
     @sections = Section.all
   end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "Section has been saved!"
+      redirect_to sections_path
+    else
+      render :new
+    end
+  end
+
+private
+  def section_params
+    params.require(:section).permit(:name)
+  end
+
+
 end
