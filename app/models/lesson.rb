b1f1ff3df2b89ccfe0d7cc1bbeaf722fc08c54cb
Kristen Kulha and Megan Olson <kristen+megan@>
2017-11-14 09:50:18 -0800

    Add next method.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,13 @@
 class Lesson < ActiveRecord::Base
   validates :name, :content, :number, :presence => true
   validates :number, uniqueness: true
+
+  def next
+    lessons = Lesson.all
+    lessons.each do |lesson|
+      if lesson.number == (self.number + 1)
+        return lesson
+      end
+    end
+  end
 end
