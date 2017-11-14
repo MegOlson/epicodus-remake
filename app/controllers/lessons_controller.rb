646b76adfe395df60d8520b44e83fc0b2cac6204
Kristen Kulha and Megan Olson <kristen+megan@>
2017-11-14 09:16:55 -0800

    Add edit.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -23,6 +23,20 @@ class LessonsController < ApplicationController
     end
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      flash[:notice] = "Your lesson as been updated!"
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
 private
   def lesson_params
     params.require(:lesson).permit(:name, :content, :number)
